0a9c0dde0acdd7aec10868f396c98b1d5e24ade8
nehagit0730 <nehabhardwaz.gtl@gmail.com>
2024-09-11 17:42:15 +0530

    three pages almost done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90c29890e6b28c06"; }
+/******/ 		__webpack_require__.h = function() { return "6185faebec2f0f49"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/data/functionalities-data.jsx
+++ components/data/functionalities-data.jsx
@@ -55,16 +55,37 @@ const functionalitiesData = [
     },
     {
         id: '#10',
+        no: '#1',
         title: 'Automated CBAM Price calculation and AI Powered price prediction',
         link: '/', 
     },
     {
         id: '#11',
+        no: '#2',
         title: 'Lower carbon price due to emission calculation based on CEMS in contrast to using default values',
         link: '/',    
     },
     {
         id: '#12',
+        no: '#3',
+        title: 'Model ARTEM Clause for better CBAM adjusted contracting',
+        link: '/',    
+    },
+    {
+        id: '#13',
+        no: '#4',
+        title: 'Automated CBAM Price calculation and AI Powered price prediction',
+        link: '/', 
+    },
+    {
+        id: '#14',
+        no: '#5',
+        title: 'Lower carbon price due to emission calculation based on CEMS in contrast to using default values',
+        link: '/',    
+    },
+    {
+        id: '#15',
+        no: '#6',
         title: 'Model ARTEM Clause for better CBAM adjusted contracting',
         link: '/',    
     },
